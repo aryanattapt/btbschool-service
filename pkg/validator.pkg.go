67cd389357198ea4a9793517f627e8e4aecdb398
Aryanatta Paramita Tiratana <aryanattapt28@outlook.co.id>
2024-09-11 20:01:13 +0700

    remove br

--- pkg/validator.pkg.go
+++ pkg/validator.pkg.go
@@ -35,45 +35,45 @@ func getMessageForTag(e validator.FieldError) string {
 	var fieldName = LowercaseFirstChar(e.StructField())
 	switch e.Tag() {
 	case "required":
-		return fieldName + " is required<br/>"
+		return fieldName + " is required"
 	case "email":
-		return fieldName + " must be a valid email address<br/>"
+		return fieldName + " must be a valid email address"
 	case "e164":
-		return fieldName + " must be a valid Phone no<br/>"
+		return fieldName + " must be a valid Phone no"
 	case "uuid":
-		return fieldName + " must be a valid UUID.<br/>"
+		return fieldName + " must be a valid UUID."
 	case "url":
-		return fieldName + " must be a valid URL.<br/>"
+		return fieldName + " must be a valid URL."
 	case "http_url":
-		return fieldName + " must be a valid HTTP URL.<br/>"
+		return fieldName + " must be a valid HTTP URL."
 	case "https_url":
-		return fieldName + " must be a valid HTTPS URL.<br/>"
+		return fieldName + " must be a valid HTTPS URL."
 	case "date":
-		return fieldName + " must be a valid date.<br/>"
+		return fieldName + " must be a valid date."
 	case "datetime":
-		return fieldName + " must be a valid datetime.<br/>"
+		return fieldName + " must be a valid datetime."
 	case "ascii":
-		return fieldName + " must contain only ASCII characters.<br/>"
+		return fieldName + " must contain only ASCII characters."
 	case "alphanum":
-		return fieldName + " must be alphanumeric.<br/>"
+		return fieldName + " must be alphanumeric."
 	case "alpha":
-		return fieldName + " must contain only alphabetic characters.<br/>"
+		return fieldName + " must contain only alphabetic characters."
 	case "number":
-		return fieldName + " must be a number.<br/>"
+		return fieldName + " must be a number."
 	case "len":
-		return fieldName + " length must be exactly as defined by the rule.<br/>"
+		return fieldName + " length must be exactly as defined by the rule."
 	case "min":
-		return fieldName + " length must be at least as defined by the rule.<br/>"
+		return fieldName + " length must be at least as defined by the rule."
 	case "max":
-		return fieldName + " length must be at most as defined by the rule.<br/>"
+		return fieldName + " length must be at most as defined by the rule."
 	case "gte":
-		return fieldName + " must be greater than or equal to as defined by the rule.<br/>"
+		return fieldName + " must be greater than or equal to as defined by the rule."
 	case "lte":
-		return fieldName + " must be less than or equal to as defined by the rule.<br/>"
+		return fieldName + " must be less than or equal to as defined by the rule."
 	case "gt":
-		return fieldName + " must be greater than as defined by the rule.<br/>"
+		return fieldName + " must be greater than as defined by the rule."
 	case "lt":
-		return fieldName + " must be less than as defined by the rule.<br/>"
+		return fieldName + " must be less than as defined by the rule."
 	default:
 		return e.Error()
 	}
